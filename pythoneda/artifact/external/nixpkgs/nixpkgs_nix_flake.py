cd9f45d62e85060c1f288f4bd9b6d42640a6b4cf
rydnr <github@acm-sl.org>
2024-02-12 15:37:18 +0100

    Fixed imports

--- pythoneda/artifact/external/nixpkgs/nixpkgs_nix_flake.py
+++ pythoneda/artifact/external/nixpkgs/nixpkgs_nix_flake.py
@@ -19,7 +19,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from .nix_flake import NixFlake
+from pythoneda.shared.nix.flake import NixFlake
 
 
 class NixpkgsNixFlake(NixFlake):
